41f2cb0d1faece6d15aef382ba8e0660d5189582
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-07-28 12:09:41 +0100

    created basic scaffold structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i.pinimg.com/736x/31/7f/30/317f3013293ec9b1aea5922548c1c664.jpg'),
+          ),
+        ),
       ),
     ),
   );
